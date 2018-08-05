0089b56eb001fcbcca52014834f22dd6505440ef
Matheus Abreu <matheus.moon@gmail.com>
2018-08-05 04:06:33 -0300

    Setters e getters da classe usuarios criados contendo algumas exeções que são: 1 - AtivUser não possui set e sim 3 tipos diferentes de add 2 - Id_foto Ainda não foi trabalhado 3 - TituloAcad returna string e ainda não diferencia genero 4 - Ainda não foi setado um getter para RedesSociasExt pois necessita-se de averiguação para um melhor comportamento
    As classes ainda não são exportadas em formulario

--- TaarapitaBase/src/java/br/com/trab/modelos/Usuario.java
+++ TaarapitaBase/src/java/br/com/trab/modelos/Usuario.java
@@ -7,6 +7,7 @@ package br.com.trab.modelos;
 
 import java.awt.Image;
 import java.awt.List;
+import java.util.ArrayList;
 
 /**
  *
@@ -19,14 +20,14 @@ public class Usuario
             nacionalidade,cep, uf, pais, instituicao, cargo;
     private Image foto;//Editar depois
     private Sexo userSexo;
-    private Data nascDat;
+    private Data nascData;
     private ClasseAcademica tituloAcad;
-    private Atividades ativUser[];
+    private ArrayList ativUser;
     private RedeSocialExternas redeSclExt;
-    private final Data nascData;
     
     public Usuario()
     {
+        this.ativUser = new ArrayList();
         nome = null;
         sobrenome = null;
         senha = null;
@@ -44,7 +45,6 @@ public class Usuario
         cargo = null;
         //idFoto = null;
         userSexo = Sexo.naoDef;
-        ativUser = null;
         redeSclExt = null;
         tituloAcad = null;
         nascData = new Data(false, true);
@@ -126,26 +126,120 @@ public class Usuario
     {
         this.userSexo = s;
     }
+    public void addAtivNoTopo(Atividades a)
+    {
+        this.ativUser.add(0, a);
+    }
+    public void addAtivNoFinal(Atividades a)/*Testar com atencao,
+            possivel dar erros*/
+    {
+        this.ativUser.add(ativUser.size(), a);
+    }
     
+    public ArrayList getLisAtiv()
+    {
+        return this.ativUser;
+    }
     
     public String getTelefone() 
     {
-        return telFixo;
+        return this.telFixo;
     }
     public String getCidade()
     {
-        return cidade;
+        return this.cidade;
     }
     public String getEndereco() 
     {
-        return endereco;
+        return this.endereco;
     }
     public String getNome() 
     {
-        return nome;
+        return this.nome;
+    }
+    public String getSobrenome() 
+    {
+        return this.sobrenome;
     }
     public String getCel() 
     {
-        return telCel;
+        return this.telCel;
+    }
+    public String getUserSexo()
+    {
+        return this.userSexo.getSexo();
+    }
+    public String getSenha()
+    {
+        return this.senha;
+    }
+    public String getEmail()
+    {
+        return this.email;
+    }
+    public String getNaturalidade()
+    {
+        return this.naturalidade;
+    }
+    public String getNacionalidade()
+    {
+        return this.nacionalidade;
+    }
+    public String getUF()
+    {
+        return this.uf;
+    }
+    public String getPais()
+    {
+        return this.pais;
+    }
+    public String getIntuicao()
+    {
+        return this.instituicao;
+    }
+    public String getCargo()
+    {
+        return this.cargo;
+    }
+    public String getTituloAcad()/*adaptar a classe para reconhecer o sexo*/
+    {
+        switch(this.tituloAcad.getClassAcad())
+        {
+            case 0:
+                return "Graduando";
+            
+            case 1:
+                
+                return "Graduado";
+            case 2:
+                return "Pós-graduando";
+                
+            case 3:
+                return "Pós-Graduado";
+                
+            case 4:
+                return "Mestrando";
+                
+            case 5:
+                return "Mestre";
+            case 6:
+                return "Doutorando";
+            case 7:
+                return "Doutor";
+            case 8:
+                return "Pós-Doutorando";
+            case 9:
+                return "Pós-Doutor";
+            default:
+                return "nd";
+        }
+    }
+    /*public getRedesSociasExt()
+        ver com o Gui a melhor maneira de exibir
+    
+    */
+    public Data getData()
+    {
+        return this.nascData;
     }
 }
